cf30c6350d37cb7989e489f3c8739e423c7c4791
Aijanss <68231963+Aijanss@users.noreply.github.com>
2020-11-14 15:02:07 +0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,32 +5,63 @@ import java.util.Collections;
 import java.util.Iterator;
 
 public class Main {
-
-    public static void main(String[] args) {
-        LineLength lineLength = new LineLength();
-        ArrayList<String> a = new ArrayList<>();
-        ArrayList<String> b = new ArrayList<>();
-        ArrayList<String> c = new ArrayList<>();
-        Iterator<String> iteratorA;
-        Iterator<String> iteratorB;
-
-        LineLength.arrayFiling(a, 5);
-        LineLength.print(a);
-
-        LineLength.arrayFiling(b, 5);
-        LineLength.print(b);
-        Collections.reverse(b);
-        iteratorA = a.iterator();
-        iteratorB = b.iterator();
-        while (iteratorA.hasNext() && iteratorB.hasNext()) {
-            c.add(iteratorA.next());
-            c.add(iteratorB.next());
+  public static void main(String[] args) {
+        System.out.println("Ведите данные");
+        System.out.println("Список А");
+        ArrayList<String> listA = new ArrayList<>(5);
+        Scanner scanner = new Scanner(System.in);
+        listA.add((scanner.nextLine()));
+        listA.add((scanner.nextLine()));
+        listA.add((scanner.nextLine()));
+        listA.add((scanner.nextLine()));
+        listA.add((scanner.nextLine()));
+        Iterator<String> iterator = listA.iterator();
+        while (iterator.hasNext()) {
+            String A = iterator.next();
+            System.out.println(A);
 
         }
-        LineLength.print(c);
-        c.sort(lineLength);
-        LineLength.print(c);
+        System.out.println("Список Б");
+        Scanner scanner1=new Scanner(System.in);
+        ArrayList<String> listB = new ArrayList<>(5);
+        listB.add(scanner1.nextLine());
+        listB.add(scanner1.nextLine());
+        listB.add(scanner1.nextLine());
+        listB.add(scanner1.nextLine());
+        listB.add(scanner1.nextLine());
+        Collections.reverse(listB);
+         iterator = listB.iterator();
+        while (iterator.hasNext()) {
+            String B = iterator.next();
+            System.out.println(B);
+        }
+        System.out.println("Общий список С");
+        ArrayList<String >listC=new ArrayList<>();
+        int i =0;
+        while (i < listC.size()){
+            listC.add(listA.get(i));
+            listC.add(listB.get(i));
+            iterator=listC.iterator();
 
+        }
+        while (iterator.hasNext()){
+            String c=iterator.next();
+            System.out.println(c);
+
+        }
+        Collections.sort(listC, new Comparator<String>() {
+            @Override
+            public int compare(String X, String L) {
+                return X.length()- L.length() ;
+            }
+        });
+        iterator=listC.iterator();
+        while (iterator.hasNext()){
+            String list=iterator.next();
+            System.out.println(list);
+        }
+    }
+ 
     }
 
 
